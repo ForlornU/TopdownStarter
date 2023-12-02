6c2d850e78c73f802635990e27a8a5bd1e667192
ForlornU <101473036+ForlornU@users.noreply.github.com>
2023-12-02 05:56:39 +0100

    Added Instructions

--- Scenes/Levels/GameUI.gd
+++ Scenes/Levels/GameUI.gd
@@ -3,5 +3,4 @@ extends Control
 @export var money_label : Label
 
 func _process(delta):
-	# Set the score label text to the score variable in game manager script
-	money_label.text = "%d" % GameManager.money
+	money_label.text = "Coins: " + "%d" % GameManager.money
