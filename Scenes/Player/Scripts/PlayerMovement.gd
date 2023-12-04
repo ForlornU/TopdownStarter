a9bbd0d12976b15130e38ae291292cbb4be82a65
ForlornU <101473036+ForlornU@users.noreply.github.com>
2023-12-04 03:11:04 +0100

    death fix

--- Scenes/Player/Scripts/PlayerMovement.gd
+++ Scenes/Player/Scripts/PlayerMovement.gd
@@ -2,9 +2,15 @@ extends CharacterBase
 class_name PlayerMain
 
 @onready var fsm = $FSM as FiniteStateMachine
+
 const DEATH_SCREEN = preload("res://Scenes/DeathScreen.tscn")
+var is_dead = false
 
 func _die():
+	if(is_dead):
+		return
+	
+	is_dead = true
 	fsm.force_change_state("Die")
 	var death_scene = DEATH_SCREEN.instantiate()
 	add_child(death_scene)
