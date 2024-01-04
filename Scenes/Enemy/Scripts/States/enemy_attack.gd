4852e48513b6908ca914bec792a3346cd4665423
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-01-04 17:36:00 +0100

    Enemy Attack Animation improvement

--- Scenes/Enemy/Scripts/States/enemy_attack.gd
+++ Scenes/Enemy/Scripts/States/enemy_attack.gd
@@ -1,22 +1,17 @@
 extends State
 class_name enemy_attack_state
 
-#@export var damage_frames : Array[int]
-var has_made_sound = false 
-var has_dealt_damage
+var has_dealt_damage #Only damage once during each attack
 
 signal DealDamage
 @export var damage = 25
 
 @onready var enemy = $"../.."
-@onready var hitbox = $"../../AnimatedSprite2D/HitBox/CollisionShape2D"
 @onready var hit_particles = $"../../AnimatedSprite2D/HitParticles"
 @export var animator : AnimationPlayer
 
 func Enter():
-	has_made_sound = false
 	has_dealt_damage = false
-	hitbox.disabled = true
 
 	animator.play("Attack")
 	await animator.animation_finished
@@ -24,12 +19,10 @@ func Enter():
 	
 func Exit():
 	has_dealt_damage = false
-	hitbox.disabled = true
 	
 #During attack animation, Hitbox is activated and tries to find the player
 func _on_hit_box_body_entered(body):
 	if body.is_in_group("Player") and has_dealt_damage == false:
-		AudioManager.play_sound(AudioManager.ENEMY_HIT, 0, -10)
 		var player = body as PlayerMain
 		deal_damage_to_player(player)
 
@@ -40,5 +33,10 @@ func deal_damage_to_player(player):
 	player.ConnectForDamage(self)
 	emit_signal("DealDamage", damage)
 	has_dealt_damage = true
+	
+func play_hitground_sound():
+	AudioManager.play_sound(AudioManager.ENEMY_HIT, 0, -10)
+	
+
 
 
