869678dd01203cc7255d9dc75f1466c5e6f28d09
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-01-06 04:36:48 +0100

    Removed has_dealt_damage

--- Scenes/Enemy/Scripts/States/enemy_attack.gd
+++ Scenes/Enemy/Scripts/States/enemy_attack.gd
@@ -1,7 +1,6 @@
 extends State
 class_name enemy_attack_state
 
-var has_dealt_damage #Only damage once during each attack
 @export var damage = 25
 
 @onready var enemy = $"../.."
@@ -9,18 +8,13 @@ var has_dealt_damage #Only damage once during each attack
 @export var animator : AnimationPlayer
 
 func Enter():
-	has_dealt_damage = false
-
 	animator.play("Attack")
 	await animator.animation_finished
 	enemy.finished_attacking()
 	
-func Exit():
-	has_dealt_damage = false
-	
 #During attack animation, Hitbox is activated and tries to find the player
 func _on_hit_box_body_entered(body):
-	if body.is_in_group("Player") and has_dealt_damage == false:
+	if body.is_in_group("Player"):
 		var player = body as PlayerMain
 		deal_damage_to_player(player)
 
@@ -28,7 +22,6 @@ func _on_hit_box_body_entered(body):
 func deal_damage_to_player(player : PlayerMain):
 	hit_particles.emitting = true
 	player._take_damage(damage)
-	has_dealt_damage = true
 	
 func play_hitground_sound():
 	AudioManager.play_sound(AudioManager.ENEMY_HIT, 0, -10)
--- Scenes/Player/Scripts/States/PlayerAttacking.gd
+++ Scenes/Player/Scripts/States/PlayerAttacking.gd
@@ -2,12 +2,10 @@ extends State
 class_name PlayerAttacking
 
 @export var animator : AnimationPlayer
-var has_dealt_damage = false
 var current_attack : Attack_Data
 @onready var hit_particles = $"../../AnimatedSprite2D/HitParticles"
 
 func Enter():
-	has_dealt_damage = false
 	AudioManager.play_sound(AudioManager.PLAYER_ATTACK_SWING, 0.3, 1)
 	
 	#Play the attack animation and wait for it to finish, transition from this state is handled by the animation player
@@ -36,11 +34,10 @@ func DetermineAttack():
 #Hitbox is turned on/off through the animationplayer, it an enemy is standing inside of it once that happens they take damage
 #Both hitboxes call back to this function through signals
 func _on_hitbox_body_entered(body):
-	if body.is_in_group("Enemy") and has_dealt_damage == false:
+	if body.is_in_group("Enemy"):
 		deal_damage(body)
 		AudioManager.play_sound(AudioManager.PLAYER_ATTACK_HIT, 0, 1)
 
 func deal_damage(enemy : EnemyMain):
 	hit_particles.emitting = true
-	has_dealt_damage = true
 	enemy._take_damage(current_attack.damage)
