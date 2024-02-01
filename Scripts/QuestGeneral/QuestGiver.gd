65b641ac79df73beb129e2c045bbe0de8371a68b
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-02-01 20:12:11 +0100

    Working Quests

--- Scripts/QuestGeneral/QuestGiver.gd
+++ Scripts/QuestGeneral/QuestGiver.gd
@@ -1,12 +1,22 @@
 extends Node
 
-#This might be a roundabout way to get a reference to the
-#quest tracker on the player
 @export var quest : Quest
 
+#When player enters: Give quest, play a sound, destroy self
 func _on_body_entered(body):
-	if body.is_in_group("Player"):
-		print("Giving Player a quest!")
-		var quest_tracker_node = body.find_child("QuestTracker") as QuestTracker
-		quest_tracker_node.start_new_quest(quest)
-	pass # Replace with function body.
+	if !body.is_in_group("Player"):
+		return
+
+	if !quest:
+		print("No quest assigned!")
+		return
+
+	give_quest(body)
+	AudioManager.play_sound(AudioManager.COIN_PICK, 0, -10)
+	queue_free()
+
+func give_quest(player):
+	print("Giving Player a quest!")
+	#This might be a roundabout way to get a reference to the quest tracker on the player..
+	var quest_tracker_node = player.find_child("QuestTracker") as QuestTracker
+	quest_tracker_node.start_new_quest(quest)
--- Scripts/QuestGeneral/QuestTracker.gd
+++ Scripts/QuestGeneral/QuestTracker.gd
@@ -10,4 +10,7 @@ func start_new_quest(new_quest : Quest):
 	quest_label.text = new_quest.quest_name + ":\n" + new_quest.quest_description
 	pass
 
+func update_quest():
+	
+	pass
 
