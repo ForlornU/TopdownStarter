80ee1d536de469dac62df19c34d8160a66a180eb
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-02-02 18:34:56 +0100

    New sound, objectives as array

--- Scripts/Managers/AudioManager.gd
+++ Scripts/Managers/AudioManager.gd
@@ -8,6 +8,7 @@ const PLAYER_ATTACK_SWING = preload("res://Art/Audio/Effects/AttackSwing.ogg")
 const ENEMY_HIT = preload("res://Art/Audio/Effects/Enemy_hit.ogg")
 const BLOODY_HIT = preload("res://Art/Audio/Effects/bloody_hit.ogg")
 const COIN_PICK = preload("res://Art/Audio/Effects/coin_pick.ogg")
+const QUEST_SOUND = preload("res://Art/Audio/Effects/QuestSound.ogg")
 
 #endregion
 var audio_player : AudioStreamPlayer 
