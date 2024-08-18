5734a3847f893101126099db92a584db55d9d682
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-08-18 21:10:10 +0200

    misc

--- Scripts/QuestGeneral/QuestGiver.gd
+++ Scripts/QuestGeneral/QuestGiver.gd
@@ -2,7 +2,7 @@ extends Node
 
 enum QuestFunction {GIVE=1, UPDATE=2, COMPLETE=3}
 @export var quest : Quest
-@export var type : QuestFunction = 1
+@export var type : QuestFunction = QuestFunction.GIVE
 var player_quest_tracker : QuestTracker
 
 #When player enters: update quest, destroy self
