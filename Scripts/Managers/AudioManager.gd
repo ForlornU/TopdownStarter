fc7f22e803ce9029927f3c3b578c5862d91ab8e5
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-08-18 02:34:18 +0200

    Audiomanager looping players

--- Scripts/Managers/AudioManager.gd
+++ Scripts/Managers/AudioManager.gd
@@ -10,33 +10,40 @@ const COIN_PICK = preload("res://Art/Audio/Effects/coin_pick.ogg")
 const QUEST_SOUND = preload("res://Art/Audio/Effects/QuestSound.ogg")
 #endregion
 
-var audio_player : AudioStreamPlayer 
-var audio_player2 : AudioStreamPlayer
+var audio_players = []
+var max_players = 8
+var starting_players = 3
 
+func _ready() -> void:
+	initiate_audio_stream()
+	
 #Play a sound, call this function from anywhere
 #offset lets you start the sound with an offset, like starting the sound at 0.1s into the clip
 #Arguments(audio_clip, offset, volume)
 #Example when calling this function:
 #AudioManager.play_sound(AudioManager.PLAYER_ATTACK_SWING, 0.25, 1)
 func play_sound(audiostream : AudioStreamOggVorbis, offset : float, volume : float):
-	if(not audio_player or not audio_player2):
-		initiate_audio_stream()
-	
-	#Play the second audioplayer if the first is already busy
-	var player = audio_player
-	if(audio_player.playing == true):
-		player = audio_player2
+	#Loop through and find an available player currently not playing a sound
+	var available_player = audio_players[0]
+	for player in audio_players:
+		if not player.is_playing():
+			available_player = player
+			break
 
-	player.stream = audiostream
-	player.pitch_scale = randf_range(0.9, 1.1)
-	player.volume_db = volume
-	player.play(offset)
+	# If no player is available and we havent reached the maximum amount of players, create a new one
+	if available_player == null and audio_players.size() < max_players:
+		available_player = AudioStreamPlayer.new()
+		audio_players.append(available_player)
+		add_child(available_player)
 
-#Instantiate two audiostreams into the scene, this only happens if none already exists
+	available_player.stream = audiostream
+	available_player.pitch_scale = randf_range(0.9, 1.1)
+	available_player.volume_db = volume
+	available_player.play(offset)
+
+#Instantiate 3 audiostreams into the scene, this only happens if none already exists
 func initiate_audio_stream():
-	audio_player = AudioStreamPlayer.new()
-	audio_player2 = AudioStreamPlayer.new()
-	add_child(audio_player)
-	add_child(audio_player2)
-	audio_player.max_polyphony = 2
-	audio_player2.max_polyphony = 2
+	for i in range(starting_players):
+		var player = AudioStreamPlayer.new()
+		audio_players.append(player)
+		add_child(player)
