467215c5f71f8581f6d8583136d15ee65e71d600
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-08-19 16:53:53 +0200

    comment

--- Scripts/AreaExit.gd
+++ Scripts/AreaExit.gd
@@ -6,10 +6,12 @@ extends Area2D
 func _ready():
 	label.visible = false
 
+#Load the next selected scene as the player presses 'Enter'
 func _process(_delta):
 	if(Input.is_action_just_pressed("Enter") and label.visible == true):
 		GameManager.load_next_level(next_scene)
 
+#Show or hide the label as the player enters and exits the area
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		label.visible = true
