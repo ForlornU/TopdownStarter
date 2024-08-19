f8337525229bd584ab5ca048ecce41e997cca814
ForlornU <101473036+ForlornU@users.noreply.github.com>
2024-08-19 16:54:05 +0200

    hardcoded firstscene load

--- Scripts/Reset.gd
+++ Scripts/Reset.gd
@@ -8,6 +8,8 @@ func _process(_delta):
 		restart()
 	if Input.is_action_just_pressed("Escape"):
 		get_tree().quit()
+	if Input.is_action_just_pressed("Enter"):
+		GameManager.load_next_level(load("res://Scenes/Levels/MainFloor.tscn")) #Hardcoded because export gave issues
 
 #When the player dies and wishes to reset, remove all of their money and reload the level
 func restart():
